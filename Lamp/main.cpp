8e3a945aafc88d60e7bb337d57b72e3d32b439a5
DAVID BENGTSSON <db96@live.se>
2019-09-19 21:29:18 +0200

    game loop and window

--- Lamp/main.cpp
+++ Lamp/main.cpp
@@ -1,6 +1,140 @@
 #include "stdafx.h"
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
-{
+// Handle to the window
+HWND hwnd = NULL;
+
+// name of the window (not the title)
+LPCTSTR WindowName = "Window name";
+
+// title of the window
+LPCTSTR WindowTitle = "Lamp";
+
+// width and height of the window
+int Width = 800;
+int Height = 600;
+
+// is window full screen?
+bool FullScreen = false;
+
+bool InitializeWindow(HINSTANCE hInstance, int ShowWnd, int width, int height, bool fullscreen);
+void gameloop();
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+
+	// create the window
+	if (!InitializeWindow(hInstance, nShowCmd, Width, Height, FullScreen))
+	{
+		MessageBox(0, "Window Initialization - Failed",
+			"Error", MB_OK);
+		return 0;
+	}
+
+	// start the main loop
+	gameloop();
+
 	return 0;
+}
+
+bool InitializeWindow(HINSTANCE hInstance,int ShowWnd,int width, int height,bool fullscreen) {
+
+	if (fullscreen) {
+
+		HMONITOR hmon = MonitorFromWindow(hwnd,
+			MONITOR_DEFAULTTONEAREST);
+		MONITORINFO mi = { sizeof(mi) };
+		GetMonitorInfo(hmon, &mi);
+
+		width = mi.rcMonitor.right - mi.rcMonitor.left;
+		height = mi.rcMonitor.bottom - mi.rcMonitor.top;
+	}
+
+	WNDCLASSEX wc;
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = NULL;
+	wc.cbWndExtra = NULL;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = WindowName;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wc)) {
+
+		MessageBox(NULL, "Error registering class",
+			"Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+
+	hwnd = CreateWindowEx(NULL,
+		WindowName,
+		WindowTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		width, height,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	if (!hwnd) {
+
+		MessageBox(NULL, "Error creating window",
+			"Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+
+	if (fullscreen) {
+		SetWindowLong(hwnd, GWL_STYLE, 0);
+	}
+
+	ShowWindow(hwnd, ShowWnd);
+	UpdateWindow(hwnd);
+
+	return true;
+}
+
+void gameloop() {
+	MSG msg;
+	ZeroMemory(&msg, sizeof(MSG));
+
+	while (true)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				break;
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			// run game code
+		}
+	}
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	
+	switch (msg)
+	{
+
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE) {
+			if (MessageBox(0, "Are you sure you want to exit?",
+				"Really?", MB_YESNO | MB_ICONQUESTION) == IDYES)
+				DestroyWindow(hwnd);
+		}
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wParam, lParam);
 }

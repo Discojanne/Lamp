64b5c3e96a84af12aab593b4ae43b9d350b4693b
DAVID BENGTSSON <db96@live.se>
2021-04-15 10:07:57 +0200

    camera cleanup

--- Lamp/Camera.h
+++ Lamp/Camera.h
@@ -13,9 +13,7 @@ public:
 	~Camera();
 
 	void init(int w, int h);
-	void BuildCamMatrices();
 	DirectX::XMMATRIX GenerateWVP(DirectX::XMMATRIX worldMatrixOfObject = DirectX::XMMatrixIdentity());
-	void UpdateCube(float dt);
 	void Update(float dt);
 
 	void OnKeyDown(int key);
@@ -42,8 +40,6 @@ private:
 
 	int m_windowWidth = 0;
 	int m_windowHeight = 0;
-	//DirectX::XMMATRIX m_cameraProjMat; // this will store our projection matrix
-	//DirectX::XMMATRIX m_cameraViewMat; // this will store our view matrix
 	float m_rotX = 0;
 	float m_rotY = 0;
 
@@ -52,19 +48,6 @@ private:
 	DirectX::XMFLOAT3 m_cameraUp; // the worlds up vector
 	float m_moveSpeed;            // Speed at which the camera moves, in units per second.
 	
-
-
-
-
-
-
-
-
-
-	DirectX::XMFLOAT4X4 m_cube1WorldMat; // our first cubes world matrix (transformation matrix)
-	DirectX::XMFLOAT4X4 m_cube1RotMat; // this will keep track of our rotation for the first cube
-	DirectX::XMFLOAT4 m_cube1Position; // our first cubes position in space
-
 };
 
 
